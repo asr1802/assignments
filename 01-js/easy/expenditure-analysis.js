26908ce9cd6f07a2557af422f7366f5de262c111
asr1802 <asr1802.info@gmail.com>
2023-12-04 17:24:34 +0530

    expenditure-analysis.js tested by npx jest

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,40 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let categorywisetotalamount = {}
+  transactions.forEach(transaction => {
+    let {category,price}=transaction
+    if(categorywisetotalamount[category]){
+      categorywisetotalamount[category]+= price
+    }else{
+      categorywisetotalamount[category]=price
+    }
+  });
+
+  console.log(Object.entries(categorywisetotalamount))
+
+  let Output= Object.entries(categorywisetotalamount).map(([category,totalAmount])=>({"category":category,"totalSpent":totalAmount}))
+  return Output
 }
+
+const Transaction = [
+  {"itemname": "Laptop", "category": "electronics", "price": 1200, "timestamp": 17064567544},
+  {"itemname": "Coffee Maker", "category": "appliances", "price": 50, "timestamp": 17064567545},
+  {"itemname": "Gaming Console", "category": "electronics", "price": 300, "timestamp": 17064567547},
+  {"itemname": "Desk Chair", "category": "furniture", "price": 150, "timestamp": 17064567548},
+  {"itemname": "Headphones", "category": "electronics", "price": 100, "timestamp": 17064567549},
+  {"itemname": "Microwave", "category": "appliances", "price": 80, "timestamp": 17064567555},
+  {"itemname": "Smartwatch", "category": "wearables", "price": 200, "timestamp": 17064567551},
+  {"itemname": "Desk Lamp", "category": "furniture", "price": 30, "timestamp": 17064567554},
+  {"itemname": "Air Purifier", "category": "appliances", "price": 120, "timestamp": 17064567558},
+  {"itemname": "Refrigerator", "category": "appliances", "price": 800, "timestamp": 17064567560},
+  {"itemname": "TV", "category": "electronics", "price": 600, "timestamp": 17064567562},
+  {"itemname": "Washing Machine", "category": "appliances", "price": 400, "timestamp": 17064567564},
+  {"itemname": "Smart Thermostat", "category": "electronics", "price": 70, "timestamp": 17064567566},
+  {"itemname": "Sofa", "category": "furniture", "price": 500, "timestamp": 17064567568},
+  {"itemname": "Robot Vacuum", "category": "appliances", "price": 250, "timestamp": 17064567570}
+]
+
+calculateTotalSpentByCategory(Transaction)
 
 module.exports = calculateTotalSpentByCategory;
