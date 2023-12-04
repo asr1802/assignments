686b4f1c5455471d9780671d5001bbaf59dbda26
asr1802 <asr1802.info@gmail.com>
2023-12-04 13:16:36 +0530

    easy - anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,32 @@
 */
 
 function isAnagram(str1, str2) {
+  console.log("String :" ,str1, str2)
+  arr1 = Array.from(str1.trim())
+  arr2 = Array.from(str2.trim())
+  if (arr1.length == arr2.length){
+    // console.log("String 1 : ",str1.split(''))
+    // console.log("String 1 : ",Object.assign([],str1))
+    // console.log("String 1 : ",Array.from(str1))
+    // console.log("String 2 : ",[...str2])
+    for (let i = 0; i<arr1.length; i++){
+      for (let j=0;j < arr2.length; j++){
+        if (arr1[i] == arr2[j]){
+          arr2.splice(arr2.indexOf(arr2[j]),1)
+        }
+      }
+    }
+    if(arr2.length == 0){
+      return true
+    }else{
+      return false
+    }
+  }else{
+    return false
+  }
 
 }
+
+console.log(isAnagram("ten","net"))
 
 module.exports = isAnagram;
